0538a2541dde9d0d921d60c61c2633af5b3700d4
Vijay S <146583628+vijayvg003@users.noreply.github.com>
2026-03-12 01:04:53 +0530

    Fix formatting issue in OTP generation method

--- secure-otp-generator/SecureOTPGenerator.java
+++ secure-otp-generator/SecureOTPGenerator.java
@@ -4,7 +4,7 @@ public class SecureOTPGenerator {
     public static String generateOTP(int length) {
         String digits = "1234567890";
         SecureRandom random = new SecureRandom();
-        StringBuilder otp = new StringBuilder();
+        StringBuilder otp = new StringBuilder(); 
         for (int i = 0; i < length; i++) {
             int index = random.nextInt(digits.length());
             otp.append(digits.charAt(index));
