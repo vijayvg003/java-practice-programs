75047cfa46802d7743c5ecc254020fe660045dbc
Vijay S <146583628+vijayvg003@users.noreply.github.com>
2026-03-13 00:05:32 +0530

    Enhance SecureOTPGenerator with comments
    Added comments to explain OTP generation method.

--- secure-otp-generator/SecureOTPGenerator.java
+++ secure-otp-generator/SecureOTPGenerator.java
@@ -1,10 +1,13 @@
 import java.security.SecureRandom;
 public class SecureOTPGenerator {
-
+    
+    // Method to generate OTP of the given length
     public static String generateOTP(int length) {
         String digits = "1234567890";
         SecureRandom random = new SecureRandom();
         StringBuilder otp = new StringBuilder(); 
+        
+        //loop to  generate random OTP
         for (int i = 0; i < length; i++) {
             int index = random.nextInt(digits.length());
             otp.append(digits.charAt(index)); 
